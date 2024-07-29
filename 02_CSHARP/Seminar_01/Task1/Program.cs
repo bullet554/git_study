b8802921a826a39338ed8a2343a1eb4d931dd427
Ilya <ilya.karpov.93@mail.ru>
2024-07-29 22:11:10 +0300

    Сортировока заданий по папкам

---
+++ 02_CSHARP/Seminar_01/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ctrl + Shift + P = autoSave
+
+Console.WriteLine("Введите первое значение: ");
+int firstValue = int.Parse(Console.ReadLine()); // Может вернуться пустая строка, если ввести не только цифры
+
+Console.WriteLine("Введите второе значение: ");
+int secondValue = Convert.ToInt32(Console.ReadLine()); // Защита от дурака. И код с терминалом чище :)
+
+ if(firstValue == secondValue * secondValue)
+ {
+    Console.WriteLine("a = " + firstValue + ", b = " + secondValue + " => Да");
+ }
+ else
+ {
+    Console.WriteLine($"a = {firstValue}, b = {secondValue} => Нет");
+ }
